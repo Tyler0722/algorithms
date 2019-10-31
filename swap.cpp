a1e563113a74b58d8d6aec4b54da458d8d2edd12
Tyler <TylerBenton0722@gmail.com>
2019-10-31 14:26:08 -0500

    built swap function

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+void swap(int *a, int *b);
+
+int main(void) {
+	int x = 1;
+	int y = 2;
+	printf("x is %i, y is %i\n", x, y);	
+	swap(&x, &y);
+	printf("x is %i, y is %i\n", x, y);
+	return 0;
+}
+
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp; 
+}
